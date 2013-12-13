58c920196d30f79c738629173b00c252c415f308
Alison Rowland <alison.rowland@businessusa.gov>
2013-12-12 23:01:31 -0500

    Got the nightly downloads working and loading into ElasticSearch via the Drakefile

--- loaders/fbo.gov/process_notices.js
+++ loaders/fbo.gov/process_notices.js
@@ -1,147 +1,128 @@
-var moment = require('moment');
-var S = require('string');
+var util = require('util');
+var datasource_id = 'FBO';
 var fs = require('fs');
+var moment = require('moment');
+//var jsonsp = require('jsonsp');
+var S = require('string');
+var transfuse = require('transfuse');
+
+var field_map = {
+	'DATE': 'posted_dt'
+	, 'SUBJECT': 'title'
+	, 'AGENCY': 'agency'
+	, 'OFFICE': 'office'
+	, 'LOCATION': 'location'
+	, 'ZIP': 'zipcode'
+	, 'SOLNBR': 'solnbr'
+	, 'RESPDATE': 'close_dt'
+	, 'DESC': 'description'
+	, 'URL': 'listing_url'
+}
+
+var tr = transfuse(function(foo) {
+    parent_notice = foo[0];
+    for (notice_type in parent_notice) {
+        // util.log('notice type: ' + notice_type);
+        notice = parent_notice[notice_type]
+        if (notice_type == 'PRESOL' || notice_type == 'COMBINE') {
+            notice_out = {};
+            notice_out.data_type = 'opp';
+            notice_out.data_source = datasource_id;
+            notice_out.notice_type = notice_type;
+
+            notice_out.solnbr = clean_solnbr(notice.SOLNBR);
+            notice_out.id = datasource_id + ':' + notice_type + ':' + notice_out.solnbr;
+
+            var mapped_field;
+            //util.log(util.inspect(notice));
+
+            for (field in notice) {
+                //util.log("field: " + field);
+
+                // skip solnbr, already handled above
+                if (field == 'SOLNBR') continue;
+
+                // skip YEAR because it gets combined with DATE instead (smh)
+                if (field == 'YEAR') continue;
+
+                // 'DESC2' is always "Link to Document"
+                if (field == 'DESC2') continue;
+
+                // skip empty fields
+                if (notice[field] == '') continue;
+
+                // get the proper field name: 
+                // some are mapped to core FBOpen Solr fields, 
+                // others simply prefixed "FBO_", 
+                // and non-core date fields get "_dt" added.
+
+                // simple_log('field = ' + field);
+
+                mapped_field = field_map[field];
+                if (mapped_field) {
+                    field_out = mapped_field;
+                } else {
+                    field_out = datasource_id + '_' + field; // prefix non-standard fields
+                    if (S(field_out).endsWith('DATE')) field_out += '_dt'; // make date fields date-friendly in Solr
+                }
+
+                // exceptions
+                if (field in ['EMAIL', 'EMAIL2']) {
+                    field_out = 'FBO_EMAIL_ADDRESS';
+                }
+                if (field == 'DESC3') { // email description always goes here (smh)
+                    field_out = 'FBO_EMAIL_DESC';
+                }
+
+                val_in = notice[field];
+                if (field == 'DATE') {
+                    val_in = val_in + notice.YEAR;
+                }
+
+                // fix up data fields
+                val_out = clean_field_value(field, val_in);
+
+                // add to the notice JSON
+                notice_out[field_out] = val_out;
+            }
+            
+            // TEST
+            //util.log('notice_out = ' + JSON.stringify(notice_out, null, "  "));
+            //util.log(util.inspect(notice_out));
+
+            return notice_out;
+        }
+
+        return 'baz';
+    }
+});
+
+var stream = fs.createReadStream('notices.json');
+stream.pipe(tr).pipe;
+tr.pipe(process.stdout);
 process.stdin.resume();
-var stdin = process.stdin;
 
-var notices = '';
-stdin.on('data', function(chunk) {
-	notices += chunk;
-});
-notices_json = {};
-stdin.on('end', function() {
-	notices_json = JSON.parse(notices);
-});
+function clean_solnbr(solnbr) {
+	return S(solnbr).trim().slugify().s;
+}
 
-var output = fs.openSync(process.argv[2], 'w');
+function solrize_date(fbo_date) {
+	// fbo_date is MMDDYYYY or MMDDYY
+	// Solr date is yyyy-MM-dd'T'HH:mm:sss'Z
+	var dt = moment(fbo_date, ['MMDDYY', 'MMDDYYYY']);
 
-function process_notices(links_output_stream, notices) {
-
-	simple_log('process_notices() ...');
-
-	// process it
-	total_processed = 0;
-	notice_count = 0;
-	simple_log(notices);
-	for (notice_idx in notices) {
-		notice = notices[notice_idx][0];
-		simple_log('notice = ' + JSON.stringify(notice, undefined, 2));
-		simple_log('notice PRESOL = ' + JSON.stringify(notice.PRESOL, undefined, 2));
-		simple_log('solnbr = ' + notice.PRESOL.SOLNBR);
-		for (notice_type in notice) {
-			simple_log('notice type = ' + notice_type);
-
-			if (notice_type == 'PRESOL' || notice_type == 'COMBINE') {
-				process_notice(notice_type, notice[notice_type]);
-				total_processed++;
-			}
-
-		}
-		notice_count++;
+	if (dt.isValid()) {
 		
+		// simple_log('fbo_date = ' + fbo_date + ', dt = ' + dt.format('YYYY-MM-DD'));
+
+		dt_out = dt.format('YYYY-MM-DD[T]HH:mm:ss[Z]');
+		return dt_out;
+	} else {
+		simple_log('WARNING: momentjs could not convert [' + fbo_date + '] into a valid date.', true);
+		return false;
 	}
 
-	simple_log('Done, total posted = ' + total_processed + ' out of ' + notice_count + ' notices in the file.', true);
-}
-process_notices(output, notices_json);
-
-function process_notice(links_output_stream, notice_type, notice) {
-
-	// build JSON for the notice
-	notice_json = build_notice_json(notice_type, notice);
-
-	// log the listing link for attachment download
-	links_output_stream.write(notice.URL + '\n');
-
-	// post it to Solr
-	// post_notice_to_solr(notice_json);
-
-}
-
-//function post_notice_to_solr(notice_json) {
-//
-//	solr_client.add(notice_json, function(err, obj) {
-//		if (err) {
-//			err_str = 'ERROR: solr_client.add failed on id = ' + notice_json.id + ', error = ' + err;
-//			console.error(err_str);
-//			simple_log(err_str, false);
-//		} else {
-//			simple_log('added: id = ' + notice_json.id + ', Solr returned ' + JSON.stringify(obj), false);
-//		}
-//	});
-//}
-
-
-function build_notice_json(notice_type, notice) {
-
-	notice_out = {};
-	notice_out.data_type = 'opp';
-	notice_out.data_source = datasource_id;
-	notice_out.notice_type = notice_type;
-
-	notice_out.solnbr = clean_solnbr(notice.SOLNBR);
-	notice_out.id = datasource_id + ':' + notice_type + ':' + notice_out.solnbr;
-
-	var mapped_field;
-
-	for (field in notice) {
-
-		// skip solnbr, already handled above
-		if (field == 'SOLNBR') continue;
-
-		// skip YEAR because it gets combined with DATE instead (smh)
-		if (field == 'YEAR') continue;
-
-		// 'DESC2' is always "Link to Document"
-		if (field == 'DESC2') continue;
-
-		// skip empty fields
-		if (notice[field] == '') continue;
-
-		// get the proper field name: 
-		// some are mapped to core FBOpen Solr fields, 
-		// others simply prefixed "FBO_", 
-		// and non-core date fields get "_dt" added.
-
-		// simple_log('field = ' + field);
-
-		mapped_field = field_map[field];
-		if (mapped_field) {
-			field_out = mapped_field;
-		} else {
-			field_out = datasource_id + '_' + field; // prefix non-standard fields
-			if (S(field_out).endsWith('DATE')) field_out += '_dt'; // make date fields date-friendly in Solr
-		}
-
-		// exceptions
-		if (field == 'EMAIL' || field == 'EMAIL2') {
-			field_out = 'FBO_EMAIL_ADDRESS';
-			// notice_out.FBO_EMAIL_ADDRESS = clean_field_value(field, notice[field]);
-		}
-		if (field == 'DESC3') { // email description always goes here (smh)
-			field_out = 'FBO_EMAIL_DESC';
-			// notice_out.FBO_EMAIL_DESC = clean_field_value(field, notice[field]);
-		}
-
-		val_in = notice[field];
-		if (field == 'DATE') {
-			val_in = val_in + notice.YEAR;
-		}
-
-		// fix up data fields
-		val_out = clean_field_value(field, val_in);
-
-		// add to the notice JSON
-		notice_out[field_out] = val_out;
-
-	}
-	
-	// TEST
-	// simple_log('notice_out = ' + JSON.stringify(notice_out, undefined, 2));
-
-	return notice_out;
-
-}
+} // solrize_date()
 
 function clean_field_value(field, val) {
 
@@ -167,52 +148,24 @@ function clean_field_value(field, val) {
 }
 
 
-function clean_solnbr(solnbr) {
-	return S(solnbr).trim().slugify().s;
-}
-
-function solrize_date(fbo_date) {
-	// fbo_date is MMDDYYYY or MMDDYY
-	// Solr date is yyyy-MM-dd'T'HH:mm:sss'Z
-	var dt = moment(fbo_date, ['MMDDYY', 'MMDDYYYY']);
-
-	if (dt.isValid()) {
-		
-		// simple_log('fbo_date = ' + fbo_date + ', dt = ' + dt.format('YYYY-MM-DD'));
-
-		dt_out = dt.format('YYYY-MM-DD[T]HH:mm:ss[Z]');
-		return dt_out;
-	} else {
-		simple_log('WARNING: momentjs could not convert [' + fbo_date + '] into a valid date.', true);
-		return false;
-	}
-
-} // solrize_date()
-
-
-
-function get_download_date_str(dt_str) {
-
-	if (dt_str) {
-		download_date_str = dt_str;
-	} else {
-		var download_dt = moment().subtract('days', 1);
-		var download_date_str = download_dt.format('YYYYMMDD');
-	}
-
-	// process.argv.forEach(function(val, idx, array) {
-	// 	// first two args are "node" and this file
-	// 	// next arg is an optional date
-	// 	if (idx == 2) download_date_str = process.argv[2];
-	// });
-
-	return download_date_str;
-}
-
-function simple_log(str, console_too) {
-	str = moment().format('YYYY-MM-DD hh:mm:ss') + ' ' + S(str).trim().s + '\n';
-	//fs.appendFileSync(log_file, str);
-	// write to console, too
-	console_too = true;
-	if (console_too) console.log(str);
-}
+// var util = require('util');
+// 
+// console.log('Starting');
+// 
+// // Intialize a new JSON stream parser.
+// //   Listen for the 'object' event, which is emitted whenever a complete JSON
+// //   object is parsed from the stream.  In this example, each object is a tweet
+// //   from the Twitter Streaming API.
+// 
+// var parser = new jsonsp.Parser()
+// parser.on('object', function(notice) {
+//     util.log(util.inspect(notice));
+// });
+// 
+// 
+// var stream = fs.createReadStream('notices.json').addListener('data', function(chunk) {
+//   // Feed each chunk of data incrementally to the JSON stream parser.  For each
+//   // complete JSON object parsed, an 'object' event will be emitted.
+//     parser.parse(chunk.toString('utf8'));
+// });
+// //stream.end();
