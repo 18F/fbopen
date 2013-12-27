beb8842e5eb4eec552ee5db64be0e40a2d3bf0d3
Alison Rowland <alison.rowland@businessusa.gov>
2013-12-27 13:38:30 -0500

    Adding first integration test to check FBO bulk loads into Elasticsearch.

---
+++ loaders/test/test_load_bulk_into_es__fbo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+PATH=$PATH:${FBOPEN_ROOT:?"FBOPEN_ROOT must be defined"}
+
+. $FBOPEN_ROOT/vendor/assert.sh/assert.sh
+
+echo "Creating a test index"
+curl -XPUT 'http://localhost:9200/fbopen_test'
+
+echo "Indexing sample/fbo.bulk in ElasticSearch"
+curl -s -XPOST 'http://localhost:9200/fbopen_test/_bulk' --data-binary @sample/fbo.bulk; echo
+
+echo "Waiting a couple seconds..."
+sleep 2
+
+# query ES via the multi get API
+# http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-multi-get.html
+# actually, let's just do a query for everything for now.
+# the multi get API wants us to specify all ids to bring back. we could write that into the test but let's be lazy.
+
+echo "Querying the ES API for what we just indexed"
+curl -s 'localhost:9200/fbopen_test/_search?size=50&pretty=true&q=*:*&fields=id' > /tmp/fbopen_output
+
+echo "Parsing out just the \"hits\" portion of the JSON"
+cat /tmp/fbopen_output | json -ag hits > /tmp/fbopen_output__hits
+cat sample/output/fbo.json | json -ag hits > sample/output/fbo__hits.json
+
+#TODO: change format_bulk.js to put the id in _id and add upsert statements. also take out is_mod from the index records
+
+# assert return contents
+echo "Asserting output (/tmp/fbopen_output__hits) equals expected (sample/output/fbo__hits.json)"
+assert "diff -q /tmp/fbopen_output__hits sample/output/fbo__hits.json" ""
+assert_end
+
+echo "Dropping the test index"
+curl -XDELETE 'http://localhost:9200/fbopen_test'
+
+echo "Done."
+
