78ac81a77e5b2f318fd76dff7baa8e9c0b5c11a4
Alison Rowland <alison.rowland@businessusa.gov>
2013-12-30 10:34:46 -0500

    Changing files that do not need to stick around to .tmp, and gitignoring them

--- loaders/test/test_load_bulk_into_es__fbo.sh
+++ loaders/test/test_load_bulk_into_es__fbo.sh
@@ -20,14 +20,14 @@ echo "Querying the ES API for what we just indexed"
 curl -s 'localhost:9200/fbopen_test/_search?size=50&pretty=true&q=*:*&fields=id' > /tmp/fbopen_output
 
 echo "Parsing out just the \"hits\" portion of the JSON"
-cat /tmp/fbopen_output | json -ag hits > /tmp/fbopen_output__hits
-cat sample/output/fbo.json | json -ag hits > sample/output/fbo__hits.json
+cat /tmp/fbopen_output | json -ag hits > /tmp/fbopen_output.tmp
+cat sample/output/fbo.json | json -ag hits > sample/output/fbo.json.tmp
 
 #TODO: change format_bulk.js to put the id in _id and add upsert statements. also take out is_mod from the index records
 
 # assert return contents
-echo "Asserting output (/tmp/fbopen_output__hits) equals expected (sample/output/fbo__hits.json)"
-assert "diff -q /tmp/fbopen_output__hits sample/output/fbo__hits.json"
+echo "Asserting output (/tmp/fbopen_output.tmp) equals expected (sample/output/fbo.json.tmp)"
+assert "diff -q /tmp/fbopen_output.tmp sample/output/fbo.json.tmp"
 assert_end
 
 echo "Dropping the test index"
