329089d2c8ec88b45f182b70c032db578c879292
Alison Rowland <alison.rowland@businessusa.gov>
2013-12-30 16:28:17 -0500

    Working grants loading test.

--- loaders/test/test_load_bulk_into_es__grants.sh
+++ loaders/test/test_load_bulk_into_es__grants.sh
@@ -17,20 +17,16 @@ sleep 2
 # the multi get API wants us to specify all ids to bring back. we could write that into the test but let's be lazy.
 
 echo "Querying the ES API for what we just indexed"
-curl -s 'localhost:9200/fbopen_test/_search?size=50&pretty=true&q=*:*&fields=id'
-# > /tmp/fbopen_output
+curl -s 'localhost:9200/fbopen_test/_search?size=50&pretty=true&q=*:*' > /tmp/fbopen_output
 
 echo "Parsing out just the \"hits\" portion of the JSON"
-#cat /tmp/fbopen_output | json -ag hits > /tmp/fbopen_output.tmp
-#cat sample/output/grants.json | json -ag hits > sample/output/grants.json.tmp
+cat /tmp/fbopen_output | json -ag hits > /tmp/fbopen_output.tmp
+cat sample/output/grants.json | json -ag hits > sample/output/grants.json.tmp
 
-#TODO: change format_bulk.js to put the id in _id and add upsert statements. also take out is_mod from the index records
+echo "Asserting output (/tmp/fbopen_output.tmp) equals expected (sample/output/grants.json.tmp)"
+assert "diff -q /tmp/fbopen_output.tmp sample/output/grants.json.tmp"
+assert_end
 
-# assert return contents
-#echo "Asserting output (/tmp/fbopen_output.tmp) equals expected (sample/output/grants.json.tmp)"
-#assert "diff -q /tmp/fbopen_output.tmp sample/output/grants.json.tmp"
-#assert_end
-#
 echo "Dropping the test index"
 curl -XDELETE 'http://localhost:9200/fbopen_test'; echo
 
