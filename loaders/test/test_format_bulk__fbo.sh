b246e20b11e3412ecce6110a0aaa0e0703db3717
Alison Rowland <alisonrowland@fastmail.fm>
2013-12-30 13:48:14 -0500

    Adding description appending to format-bulk and making it an option.

--- loaders/test/test_format_bulk__fbo.sh
+++ loaders/test/test_format_bulk__fbo.sh
@@ -5,10 +5,10 @@ source setup.sh
 echo "Formatting standard JSON into Elasticsearch bulk format JSON"
 # http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-bulk.html
 
-cat sample/fbo_preprocessed.json | node $FBOPEN_ROOT/loaders/common/format-bulk.js > /tmp/fbopen_output
+cat sample/fbo_preprocessed.json | node $FBOPEN_ROOT/loaders/common/format-bulk.js -a > /tmp/fbopen_output
 
 # make sure file is not empty
-assert "wc -l /tmp/fbopen_output" "12 /tmp/fbopen_output"
+assert "wc -l /tmp/fbopen_output" "18 /tmp/fbopen_output"
 # then check contents are correct
 assert "diff -q /tmp/fbopen_output sample/output/fbo.bulk"
 
