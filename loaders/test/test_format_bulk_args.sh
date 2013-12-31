79024674b5ad05e74baa9391c373383f4cf4dc3c
Alison Rowland <alison.rowland@businessusa.gov>
2013-12-31 10:48:01 -0500

    Adding a test stub for testing format-bulk arguments

---
+++ loaders/test/test_format_bulk_args.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source setup.sh
+
+sample='{"this":"is a sample doc"}'
+
+echo $sample | node $FBOPEN_ROOT/loaders/common/format-bulk.js > /tmp/fbopen_output
+
+echo "Note this test is just a stub for now."
+assert "echo \"{}\" | diff -q - /tmp/fbopen_output" ""
+# TODO: trying to work out if i can embed a multi-line result in the assert...
+#assert "echo << END_TEST
+#{"index":{"_type":"type1"}}
+#{"this":"is a sample doc"}
+
+assert_end
+
+
