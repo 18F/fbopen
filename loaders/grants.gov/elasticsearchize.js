017388a9ed9406b9bc859a1c4d76f1cbfb94d9b3
Alison Rowland <alison.rowland@businessusa.gov>
2014-01-03 11:07:28 -0500

    Removing non-working file

--- loaders/grants.gov/elasticsearchize.js
+++
@@ -1,9 +0,0 @@
-// load STDIN
-
-var big_xml = require('big-xml');
-
-var reader = big_xml.createReader(process.argv[1], /^doc$/);
-
-reader.on('record', function(record) {
-    process.stdout.write(JSON.stringify(record));
-});
