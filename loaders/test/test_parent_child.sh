bf157bac16cf0d6d15b91c8158a935bacea235a2
Alison Rowland <alison.rowland@businessusa.gov>
2014-01-16 13:49:57 -0500

    With some caveats, attachment indexing is working on this test.

--- loaders/test/test_parent_child.sh
+++ loaders/test/test_parent_child.sh
@@ -2,39 +2,50 @@
 
 source setup.sh
 
+echo "Delete index so we can start out clean"
+curl -XDELETE 'http://localhost:9200/fbopen_test'
+
 echo "Creating a test index"
 curl -XPUT 'http://localhost:9200/fbopen_test' -d '{
-    "settings" : {
-        "number_of_shards" : 1
-    },
-    "mappings" : {
-        "opp" : {
-        },
-        "opp_attachment" : {
-            "_parent" : "opp",
-            "properties" : {
-                "content" : {
-                    "type" : "attachment",
+        "mappings" : {
+            "opp" : {
+            },
+            "opp_attachment" : {
+                "_parent" : { "type" : "opp" },
+                "properties" : {
+                    "content" : {
+                        "type" : "attachment",
+                        "fields" : {
+                            "content"  : { "store" : "no" },
+                            "author"   : { "store" : "yes" },
+                            "title"    : { "store" : "yes", "analyzer" : "english"},
+                            "date"     : { "store" : "yes" },
+                            "keywords" : { "store" : "yes", "analyzer" : "keyword" },
+                            "_name"    : { "store" : "yes" },
+                            "_content_type" : { "store" : "yes" }
+                        }
                     }
                 }
             }
         }
-    }
 }'
 
 echo "Indexing sample/fbo.opp_type.bulk in ElasticSearch"
 curl -s -XPOST 'http://localhost:9200/fbopen_test/_bulk' --data-binary @sample/fbo.opp_type.bulk; echo
 
-echo "Waiting for indexing..."
-sleep 2
+echo "Waiting for bulk indexing... (1s)"
+sleep 1
 
 echo "Indexing each attachment"
 file=$(openssl base64 -in sample/attachments/test.rtf)
-json="{\"content\" : \"${file}\", \"parent\" : \"FBO:PRESOL:14-0004\"}"
-echo $json | curl -i -XPOST 'http://localhost:9200/fbopen_test/opp_attachment' --data-binary @-
+json="{\"_name\" : \"test.rtf\", \"content\" : \"${file}\"}"
+echo $json | curl -i -XPOST 'http://localhost:9200/fbopen_test/opp_attachment?parent=FBO:PRESOL:14-0004' -d @-
+
+echo "Waiting for attachment indexing... (10s)"
+sleep 10
 
 echo "Querying the ES API for what we just indexed"
-curl 'localhost:9200/fbopen_test/_search?size=50&pretty=true&q=*:*&fields=id'# > /tmp/fbopen_output
+curl 'localhost:9200/fbopen_test/_search?pretty=true&q=john'
 exit
 
 echo "Parsing out just the \"hits\" portion of the JSON"
