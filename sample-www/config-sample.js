ca42234b97d8ca23172cab26393a3a4992a551b9
Aaron Snow <aaronsnow@gmail.com>
2014-01-26 12:12:53 -0500

    move config to main dir; add sample config file; simplify js include paths

--- sample-www/config-sample.js
+++ sample-www/config-sample.js
@@ -1,10 +1,12 @@
+// Configure for your API server below.
+// This is also a good place to put server-specific code, e.g., analytics snippets.
+
 (function( fbopen, $, undefined ) { // http://stackoverflow.com/q/5947280/185839
   
   fbopen_config = {
-  	// API_SERVER: '//localhost:3000' // Point to wherever you're running your fbopen API server
-  	API_SERVER: 'http://YOUR.API.SERVER' // Point to wherever you're running your fbopen API server
+  	API_SERVER: '//localhost:3000' // Point to wherever you're running your fbopen API server
   	, API_PARAMS: { // any additional parameters that need to be passed to the API server, e.g., an api key
-  		'api_key': 'YOUR_KEY_HERE'
+  		// 'api_key': 'YOUR_API_KEY'
   	} 
   }
 
