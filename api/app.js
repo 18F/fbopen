a27745ee9b05017344f75461fb9e946a761b92ad
Alison Rowland <alison.rowland@businessusa.gov>
2014-03-17 15:52:18 -0400

    Bringing API into line with less-middleware >= 0.2.1. NOTE: Requires command npm update less-middleware

--- api/app.js
+++ api/app.js
@@ -55,7 +55,7 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(require('less-middleware')(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
