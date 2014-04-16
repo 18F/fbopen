9f3b52516242ad63a99105f33f328a2f62784b80
arowla <alison.rowland@gsa.gov>
2014-04-16 15:44:05 -0400

    Squashing a bug hidden in this attachments test. Now querying for totally unique terms. Querying parent via the child now works, too. And we stop storing the base64 encoded file contents.

--- loaders/test/test_attachment_load_and_search.sh
+++ loaders/test/test_attachment_load_and_search.sh
@@ -7,21 +7,23 @@ curl -XDELETE 'http://localhost:9200/fbopen_test'
 
 echo "Creating a test index"
 curl -XPUT 'http://localhost:9200/fbopen_test' -d '{
+        "settings" : { "index": {"number_of_shards" : 1, "number_of_replicas" : 0 } },
         "mappings" : {
             "opp" : {
             },
             "opp_attachment" : {
                 "_parent" : { "type" : "opp" },
+                "_source" : { "excludes" : [ "content" ] },
                 "properties" : {
                     "content" : {
                         "type" : "attachment",
                         "fields" : {
-                            "content"  : { "store" : "no" },
-                            "author"   : { "store" : "yes" },
-                            "title"    : { "store" : "yes", "analyzer" : "english"},
-                            "date"     : { "store" : "yes" },
-                            "keywords" : { "store" : "yes", "analyzer" : "keyword" },
-                            "_name"    : { "store" : "yes" },
+                            "content"       : { "store" : "no" },
+                            "author"        : { "store" : "no" },
+                            "title"         : { "store" : "no", "analyzer" : "english" },
+                            "date"          : { "store" : "no" },
+                            "keywords"      : { "store" : "no", "analyzer" : "keyword" },
+                            "_name"         : { "store" : "no" },
                             "_content_type" : { "store" : "yes" }
                         }
                     }
@@ -42,33 +44,41 @@ FBOPEN_URI=localhost:9200 FBOPEN_INDEX=fbopen_test $FBOPEN_ROOT/loaders/common/l
 echo "Waiting for attachment indexing... (5s)"
 sleep 5
 
-echo "Querying the ES API for what we just indexed"
-curl 'localhost:9200/fbopen_test/opp/_search' -d '{
+echo "Querying attachments directly for 'Jamiroquai'"
+curl 'localhost:9200/fbopen_test/opp_attachment/_search?pretty=true' -d '{
+    "query" : {
+        "match" : { "_all" : "Jamiroquai" }
+    }
+}'  
+
+echo "Querying the ES API for any field in an opp or opp_attachment containing 'Jamiroquai'"
+curl 'localhost:9200/fbopen_test/opp/_search?pretty=true' -d '{
     "query" : {
         "bool" : {
             "should" : [
-                { "match" : { "_all" : "john" } },
+                { "match" : { "_all" : "Jamiroquai" } },
                 { "has_child" : {
                     "type" : "opp_attachment",
-                    "query" : { "term" : { "_all" : "john" } }
+                    "query" : { "match" : { "_all" : "Jamiroquai" } }
                 } }
             ]
         }
     }
 }'
-exit
 
-echo "Parsing out just the \"hits\" portion of the JSON"
-cat /tmp/fbopen_output | json -ag hits > /tmp/fbopen_output.tmp
-cat sample/output/fbo.json | json -ag hits > sample/output/fbo.json.tmp
-
-# assert return contents
-echo "Asserting output (/tmp/fbopen_output.tmp) equals expected (sample/output/fbo.json.tmp)"
-assert "diff -q /tmp/fbopen_output.tmp sample/output/fbo.json.tmp"
-assert_end
-
-echo "Dropping the test index"
-curl -XDELETE 'http://localhost:9200/fbopen_test'; echo
+echo "Querying the ES API for any field in an opp or opp_attachment containing 'narwhal'"
+curl 'localhost:9200/fbopen_test/opp/_search?pretty=true' -d '{
+    "query" : {
+        "bool" : {
+            "should" : [
+                { "match" : { "_all" : "narwhal" } },
+                { "has_child" : {
+                    "type" : "opp_attachment",
+                    "query" : { "match" : { "_all" : "narwhal" } }
+                } }
+            ]
+        }
+    }
+}'
 
 echo "Done."
-
