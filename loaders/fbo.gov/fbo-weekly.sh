c9b0fc5ad644437ad1d09ee2214e6f8827334a80
arowla <alison.rowland@gsa.gov>
2014-06-10 02:52:14 -0400

    For FBO weekly dump, only get attachments for open solicitations

--- loaders/fbo.gov/fbo-weekly.sh
+++ loaders/fbo.gov/fbo-weekly.sh
@@ -7,6 +7,7 @@
 # you can specify just the input file, or all four files, or none at all
 
 json_output_file='workfiles/notices.json'
+open_json_output_file='workfiles/notices_open.json'
 links_output_file='workfiles/listings-links.txt'
 bulk_output_file='workfiles/notices.bulk'
 
@@ -34,8 +35,11 @@ wget ftp://ftp.fbo.gov/datagov/FBOFullXML.xml -O $FBO_WEEKLY_XML_FILE
 echo "Converting to JSON..."
 cat $FBO_WEEKLY_XML_FILE | node process_notices__weekly.js > $json_output_file
 
-echo "Extracting links..."
-cat $json_output_file | json -ga listing_url > $links_output_file
+echo "Finding open notices..."
+cat $json_output_file | node $FBOPEN_ROOT/loaders/common/get_open_listings.js > $open_json_output_file
+
+echo "Extracting links from open notices..."
+cat $open_json_output_file | json -ga listing_url > $links_output_file
 
 echo "Converting JSON to Elasticsearch bulk format..."
 # Note the need to split the file into chunks. Else ES will fail with either a
