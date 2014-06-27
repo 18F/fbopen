ee0100a7df5be7d8c515b39f37dd95d158d6812b
arowla <alison.rowland@gsa.gov>
2014-06-27 13:15:31 -0400

    Set up Elasticsearch properly and break version out into a constant

--- initial-dev-setup.sh
+++ initial-dev-setup.sh
@@ -3,6 +3,9 @@ command_exists () {
     type "$1" &> /dev/null ;
 }
 
+ES_VERSION=1.2.1
+FBOPEN_ROOT=`pwd`
+
 echo "Initial setup"
 ./setup.sh
 
@@ -16,15 +19,19 @@ else
 fi
 
 # install ES if it doesn't already exist
-if [ -d ../elasticsearch-1.2.1.zip ]; then
+if [ -d ../elasticsearch-$ES_VERSION.zip ]; then
   echo "Elasticsearch is already installed."
 else
   echo "Installing Elasticsearch"
-  wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.zip -P ../
-  unzip ../elasticsearch-1.2.1.zip -d ../
-  rm -rf ../elasticsearch-1.2.1.zip 
+  wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.zip -P ../
+  unzip ../elasticsearch-$ES_VERSION.zip -d ../
+  rm -rf ../elasticsearch-$ES_VERSION.zip
 fi
 
+# use custom config
+cp elasticsearch/elasticsearch__dev.yml elasticsearch-$ES_VERSION/config/elasticsearch.yml
+# install mapper-attachments
+elasticsearch-$ES_VERSION/bin/plugin -install elasticsearch/elasticsearch-mapper-attachments/2.0.0
 
 echo "Starting Elasticsearch"
 osascript<<EOF
@@ -33,9 +40,12 @@ tell application "System Events"
 end
 tell application "Terminal"
   activate
-  do script with command "fbopen/elasticsearch-1.2.1/bin/elasticsearch" in window 1
+  do script with command "fbopen/elasticsearch-$ES_VERSION/bin/elasticsearch" in window 1
 end tell
 EOF
+
+# init index with mappings and settings
+curl -XPUT localhost:9200/fbopen0 --data-binary @elasticsearch/init.json
 
 # API
 echo "Creating api/config.js from sample."
@@ -45,6 +55,7 @@ if command_exists node ; then
   echo
 else
   echo "It looks like node.js is not installed. Please install it."
+  exit 1
 fi
 
 npm install api/
@@ -59,8 +70,7 @@ npm install loaders/bids.state.gov/
 npm install loaders/common/
 
 # problems:
-#FBOPEN_ROOT=`cd ..; pwd` loaders/bids.state.gov/bids-nightly.sh
 cd loaders/bids.state.gov
-FBOPEN_ROOT=~/fbopen/fbopen/ FBOPEN_URI=localhost:9200 FBOPEN_INDEX=fbopen ./bids-nightly.sh
+FBOPEN_ROOT=$FBOPEN_ROOT FBOPEN_URI=localhost:9200 FBOPEN_INDEX=fbopen ./bids-nightly.sh
 cd ../..
 
