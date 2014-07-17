efb0e1d857bd30ce5f3b97b699ae618637f38a35
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-17 13:42:40 -0400

    added weekly loader script, bamboo config updated

---
+++ loaders/fbo.gov/fbo-load-weekly-attachments.sh
@@ -0,0 +1,8 @@
+
+weekly_links_file="$FBOPEN_ROOT/loaders/fbo.gov/workfiles/listings-links.txt"
+
+echo "Starting attachment scrape/load. See ~/log/fbo_attach.log for more info..."
+cd $FBOPEN_ROOT/loaders/attachments
+python fbo.py run --file $weekly_links_file --log-to-stdout
+
+
