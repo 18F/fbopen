55b84ef6b9a3015cdac93055c81d6608fa0701be
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-18 16:35:02 -0400

    updated disk usage

--- tools/bamboo/disk_usage.py
+++ tools/bamboo/disk_usage.py
@@ -5,3 +5,5 @@ parts = psutil.disk_partitions()
 for p in parts:
     percent = psutil.disk_usage(p.mountpoint).percent
     print("Disk {0} is {1:.2f} percent full".format(p.mountpoint, percent))
+    if percent > 75:
+        raise Exception('LOW DISK SPACE on volume {}'.format(p.mountpoint), "WARNING LOW DISK SPACE")
