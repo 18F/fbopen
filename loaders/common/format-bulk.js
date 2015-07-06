ec1dea2b3b6f876a1e98d10ba68d1f4d5fae972b
arowla <alison.rowland@gsa.gov>
2015-07-06 15:25:59 -0400

    Changed custom script name (for CF project namespacing)

--- loaders/common/format-bulk.js
+++ loaders/common/format-bulk.js
@@ -78,7 +78,7 @@ var bulkify_data = function (data) {
             description = _delete(data, 'description');
             // we may need to figure out how to bypass this for grants
             descrip_obj = {};
-            descrip_obj['script'] = "loaders_append_desc";
+            descrip_obj['script'] = "fbopen_append_desc";
             descrip_obj['params'] = { "description": description, "posted_dt": data['posted_dt'] };
 
             lines.push(JSON.stringify(update_command(id)));
