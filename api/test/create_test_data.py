285ccf728a310d7578f90e7a1d44177d4f515c47
arowla <alison.rowland@gsa.gov>
2015-09-01 22:55:30 -0400

    Updated create_test_data script
    - Imported `sys`, the absence of which was causing the script to only use the default
      index name
    - Uppercased script-wide variables/constants
    - Changed data structure to output object-per-line JSON, instead of an
      array (that seems to be all Elasticdump will accept)

--- api/test/create_test_data.py
+++ api/test/create_test_data.py
@@ -1,11 +1,17 @@
 from elasticsearch import Elasticsearch
 import json
+import sys
 
 try:
-    fbopen_index = sys.argv[1]
+    FBOPEN_INDEX = sys.argv[1]
 except:
-    fbopen_index = 'fbopen0'
+    FBOPEN_INDEX = 'fbopen0'
     print("Warning: Using fbopen0 as default index since none provided")
+
+try:
+    SKIP_NUM = int(sys.argv[2])
+except:
+    SKIP_NUM = 10
 
 
 test_data = []
@@ -15,15 +21,14 @@ outfile = open('data/test_data.json', 'w')
 
 for data_type in ('opp', 'opp_attachment'):
     #get opps 
-    resp = es.search(index=fbopen_index, doc_type=data_type, body={"query":{"match_all":{}}})
+    resp = es.search(index=FBOPEN_INDEX, doc_type=data_type, body={"query":{"match_all":{}}})
     num = 0 
     total = resp['hits']['total']
 
     while num < total :
-        rec = es.search(index=fbopen_index, doc_type=data_type, body={"size": 1, "from": num, "query":{"match_all":{}}})
+        rec = es.search(index=FBOPEN_INDEX, doc_type=data_type, body={"size": 1, "from": num, "query":{"match_all":{}}})
         obj = rec['hits']['hits'][0]
         test_data.append(json.dumps(obj))
-        num += 10
+        num += SKIP_NUM
         
-outfile.write('[' + '\n,'.join(test_data)  + ']')
-
+outfile.write('\n'.join(test_data))
