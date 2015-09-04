15f75b7eeed08b1beb4dae6860ec7dcd52361d84
arowla <alison.rowland@gsa.gov>
2015-09-03 22:17:32 -0400

    Updated FBO loader
    - Added custom New Relic background process instrumentation
    - Changed the cron timing back to nightly

--- loaders/fbo.gov/cron.js
+++ loaders/fbo.gov/cron.js
@@ -1,4 +1,3 @@
-
 var CronJob = require('cron').CronJob,
   config = require('../config'),
   spawn = require('child_process').spawn,
@@ -7,51 +6,50 @@ var CronJob = require('cron').CronJob,
   nr = require('newrelic'),
   child_running = false;
 
-var loaderNightly = function () {
+var loaderNightly = nr.createBackgroundTransaction('loader:fbo-nightly:check', function() {
   if (!child_running) {
+      // end the "check" transaction before starting the "spawn" one
+      nr.endTransaction(); // loader:fbo-nightly:check
       child_running = true;
-      nr.createBackgroundTransaction('spawn:fbo-nightly', function() {
-        spawnLoader(function(err) {
-          if (err) {
-            child_running = false;
-            console.error(err);
-          }
-          nr.endTransaction();
-        });
+      spawnLoader(function(err) {
+        if (err) console.error(err);
+        child_running = false;
+        nr.endTransaction(); // loader:fbo-nightly:spawn
       });
   } else {
+    nr.endTransaction(); // loader:fbo-nightly:check
     console.log('Still running...');
   }
-};
+});
 
-var spawnLoader = function (callback) {
-  var child = spawn(path.join(__dirname, 'fbo-nightly.sh'), [], {
-    detached: true,
-    stdio: 'inherit',
-    env: {
-      HOME: process.env.HOME,
-      FBOPEN_URI: config.elasticsearch.uri,
-      PATH: process.env.PATH,
-    }
-  });
-  // child.stdout.on('data', function(data) { console.log(data.toString()); });
-  // child.stderr.on('data', function(data) { console.error(data.toString()); });
-  // these never seem to be called with this script
-  // child.on('error', function(err) { callback(err); });
-  // child.on('exit', function(code, signal) { callback(); });
-  child.on('close', function(code) {
-    if (code !== 0) {
-      callback('Script returned non-zero exit status');
-    } else {
-      callback();
-    }
-  });
+var spawnLoader = nr.createBackgroundTransaction('loader:fbo-nightly:spawn', function (callback) {
+  var child = nr.createTracer('loader:fbo-nightly:start', function() {
+    spawn(path.join(__dirname, 'fbo-nightly.sh'), [], {
+      detached: true,
+      stdio: 'inherit',
+      env: {
+        HOME: process.env.HOME,
+        FBOPEN_URI: config.elasticsearch.uri,
+        PATH: process.env.PATH,
+      }
+    }).on('data', function(data) { console.log(data.toString()); })
+    .on('error', function(err) { callback(err); })
+    .on('exit', function(code, signal) { callback(); })
+    .on('close', nr.createTracer('loader:fbo-nightly:finishing', function(code) {
+      if (code !== 0) {
+        callback('Script returned non-zero exit status');
+      } else {
+        callback();
+      }
+    }))
+  })();
 
   if (child) console.log('Started loader!');
-}
+});
 
 var cronjob = new CronJob({
-    cronTime: '*/10 * * * * *',
+    cronTime: '30 12 * * *',
+    // cronTime: '*/10 * * * * *', // for debugging
     onTick: loaderNightly,
     timeZone: 'America/New_York'
 });
