d3fe2c67a2556cf5e931f0841e5f015f1d85f5be
Colin Craig <colin.craig@gsa.gov>
2016-01-13 12:18:44 -0500

    Added initial Docker setup for ES

--- api/config.js
+++ api/config.js
@@ -1,5 +1,4 @@
 // Create config.js, in this directory, based on this sample file.
-var appEnv = require('cfenv').getAppEnv();
 
 fs = require('fs');
 
@@ -14,54 +13,24 @@ config.app.read_only = true;
 
 // require basic authentication
 config.app.require_http_basic_auth = false;
-config.app.http_basic_auth = {
-	realm: 'fbopen'
-	, file: '/path/to/htpasswd_file' // username/password file created using htpasswd
-};
 
 // http
 config.app.listen_http = true;
-config.app.port = process.env.PORT || 3000;
+config.app.port = process.env.FBOPEN_API_PORT || 3000;
 
 // https
 config.app.listen_https = false;
 
 // logger
 config.logger = {};
-config.logger.path = __dirname + '/../log/api.log';
-
-// settings from cloud foundry env
-cf_services = JSON.parse((process.env.VCAP_SERVICES || '{}'));
+config.logger.path = '/var/log/fbopen/api.log';
 
 // elasticsearch
-// henceforth, all ES service instances shall be named "es-YYYYMMDD",
-// with the date being the day they were instantiated
-config.elasticsearch = appEnv.getServiceCreds("es-.*");
-// the above defines the following keys: [ hostname, password, port, uri, username ]
-
-// if running locally
-if (!config.elasticsearch) {
-  config.elasticsearch = {
-    uri: 'http://localhost:9200',
-    hostname: 'localhost',
-    port: 9200
-  };
-}
-
-console.log('[API] Discovered elasticsearch config: ', config.elasticsearch);
-
+config.elasticsearch = {};
+config.elasticsearch.host = process.env.ELASTICSEARCH_HOST || 'localhost';
+config.elasticsearch.port = '9200';
 config.elasticsearch.index = process.env.ELASTICSEARCH_INDEX || 'fbopen';
 // this is used to adjust the date math that's used for searching within the (statically-dated) test data
 config.elasticsearch.now_str = process.env.ELASTICSEARCH_NOW || 'now';
-
-// Sentry config
-if (process.env.env !== 'development') {
-  config.sentry_uri = appEnv.getServiceURL('sentry');
-
-  if (!config.sentry_uri) {
-    console.error('The Sentry URI could not be found.');
-    process.exit(1);
-  }
-}
 
 module.exports = config;
